f90f495d69bd69f1c902537653d0f519fcbd9b7a
James-SwiftUI <jamesholmson@gmail.com>
2024-01-03 14:11:48 +0000

    Initial Commit

---
+++ HospitalCharts/HospitalChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HospitalChartsApp.swift
+//  HospitalCharts
+//
+//  Created by James on 03/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct HospitalChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
